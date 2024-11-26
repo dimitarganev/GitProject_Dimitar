4869f98b25b9495d404d726deffcd6e2162eb94f
unknown <dganev6@gmail.com>
2024-11-26 09:52:42 +0200

    test

---
+++ GitProject2_Dimitar/GitProject_Dimitar/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Прост калкулатор на C#");
+        Console.WriteLine("Въведете първото число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Въведете операция (+, -, *, /):");
+        char operation = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.WriteLine("Въведете второто число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                    Console.WriteLine("Грешка: Деление на нула не е позволено.");
+                break;
+            default:
+                Console.WriteLine("Невалидна операция.");
+                break;
+        }
+
+        if (operation == '+' || operation == '-' || operation == '*' || (operation == '/' && num2 != 0))
+        {
+            Console.WriteLine($"Резултат: {num1} {operation} {num2} = {result}");
+        }
+
+        Console.WriteLine("Натиснете произволен клавиш за изход...");
+        Console.ReadKey();
+    }
+}
